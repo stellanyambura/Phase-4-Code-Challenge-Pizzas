53e324d51fbf4e8b773ffb72332617a111524885
Stellanyambura <stellagitonga.com>
2023-03-20 21:39:33 +0300

    eddited file

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,14 +4,14 @@
       render json: restaurants, only: [:id, :name, :address]
      end
   
-#     def show
-#       restaurant = Restaurant.find_by(id: params[:id])
-#       if restaurant
-#         render json: restaurant.as_json(include: { pizzas: { only: [:id, :name, :ingredients] } })
-#       else
-#         render json: { error: "Restaurant not found" }, status: :not_found
-#       end 
-#     end 
+    def show
+       restaurant = Restaurant.find_by(id: params[:id])
+       if restaurant
+        render json: restaurant.as_json(include: { pizzas: { only: [:id, :name, :ingredients] } })
+       else
+         render json: { error: "Restaurant not found" }, status: :not_found
+       end 
+    end 
   
 #     def destroy
 #       restaurant = Restaurant.find_by(id: params[:id])
