9b8c7c98c598123002ef66890c17280d8bcc69f8
Stellanyambura <stellagitonga.com>
2023-03-20 21:45:02 +0300

    eddited file

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -30,20 +30,20 @@
     end
   end
   
-#   class RestaurantPizzasController < ApplicationController
-#     def create
-#       restaurant_pizza = RestaurantPizza.new(restaurant_pizza_params)
-#       if restaurant_pizza.save
-#         render json: restaurant_pizza.pizza.slice(:id, :name, :ingredients)
-#       else
-#         render json: { errors: restaurant_pizza.errors.full_messages }, status: :unprocessable_entity
-#       end
-#     end
+  class RestaurantPizzasController < ApplicationController
+    def create
+             restaurant_pizza = RestaurantPizza.new(restaurant_pizza_params)
+                    if restaurant_pizza.save
+        render json: restaurant_pizza.pizza.slice(:id, :name, :ingredients)
+       else
+         render json: { errors: restaurant_pizza.errors.full_messages }, status: :unprocessable_entity
+       end
+    end
   
-#     private
+     private
   
-#     def restaurant_pizza_params
-#       params.require(:restaurant_pizza).permit(:price, :pizza_id, :restaurant_id)
-#     end
+   def restaurant_pizza_params
+      params.require(:restaurant_pizza).permit(:price, :pizza_id, :restaurant_id)
+    end
    end
   
