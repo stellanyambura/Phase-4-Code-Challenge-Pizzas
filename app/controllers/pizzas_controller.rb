1c9a7462c632564dabe7fcd063cc715888e68f15
Stellanyambura <stellagitonga.com>
2023-03-21 10:08:50 +0300

    fixed bugs

---
+++ app/controllers/pizzas_controller.rb
@@ -0,0 +1,6 @@
+class PizzasController < ApplicationController
+    def index
+        pizza = Pizza.all
+        render json: pizza, only: [:id, :name, :ingredients]
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
  Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show]
+  resources :pizzas, only: [:index]
+
 end
